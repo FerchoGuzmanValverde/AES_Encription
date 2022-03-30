07fe7f49419be7745812bec7a8344c071b9d9991
FerchoGuzmanValverde <gvf0026694@est.univalle.edu>
2022-03-29 22:05:36 -0400

    Last version

---
+++ AES_Encryption/AES.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace AES_Encryption
+{
+    internal class AES
+    {
+        //Encryption Key
+        static byte[] Key = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
+            17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32};
+        //Initialization vector
+        static byte[] IV = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
+        static byte[] encriptado;
+        static AesManaged aes = new AesManaged();
+
+        static string encriptadoAES(string cadena)
+        {
+            string salida = "";
+            // Generación de la llave de encriptación y del vector de inicialización
+            Key = aes.Key;
+            IV = aes.IV;
+            try
+            {
+                using (aes)
+                {
+                    encriptado = Encriptar(cadena);
+                    salida = $"{Encoding.UTF8.GetString(encriptado)}";
+                }
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine("Excepcion: " + exp.Message);
+                Console.ReadKey();
+            }
+            return salida;
+        }
+
+        public static byte[] Encriptar(string texto)
+        {
+            byte[] encriptando;
+            using (AesManaged aes = new AesManaged())
+            {
+                ICryptoTransform encriptador = aes.CreateEncryptor(Key, IV);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encriptador, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter sw = new StreamWriter(cs))
+                            sw.Write(texto);
+                        encriptando = ms.ToArray();
+                    }
+                }
+            }
+            return encriptando;
+        }
+        public static string Desencripta(byte[] cifrado)
+        {
+            string texto = null;
+            using (AesManaged aes1 = new AesManaged())
+            {
+                ICryptoTransform desencriptar = aes1.CreateDecryptor(Key, IV);
+                using (MemoryStream ms = new MemoryStream(cifrado))
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, desencriptar, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader reader = new StreamReader(cs))
+                            texto = reader.ReadToEnd();
+                    }
+                }
+            }
+            return texto;
+        }
+    }
+}
---
+++ AES_Encryption/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
+
+namespace AES_Encryption
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        OpenFileDialog ofd;
+        string file;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEncrypt_Click(object sender, RoutedEventArgs e)
+        {
+            loadFile("Text files (*.txt)|*.txt");
+            string encrypt = "";
+
+            if (ofd.ShowDialog() == true)
+            {
+                file = ofd.FileName;
+                lblMsg.Content = "The file is: " + file;
+                encrypt = File.ReadAllText(file);
+                byte[] encrypted = AES.Encriptar(encrypt);
+                File.WriteAllBytes(file.Replace(".txt", "_Encript.data"), encrypted);
+                lblMsg.Content = "The process has finished...";
+            }
+        }
+
+        private void btnDecrypt_Click(object sender, RoutedEventArgs e)
+        {
+            loadFile("Data files (*.data)|*.data");
+            string decrypted = "";
+            if (ofd.ShowDialog() == true)
+            {
+                file = ofd.FileName;
+
+                lblMsg.Content = "Current file: " + file;
+                byte[] decrypt = File.ReadAllBytes(file);
+                decrypted = AES.Desencripta(decrypt);
+                File.WriteAllText(file.Replace("_Encript.data", "_Decript.txt"), decrypted);
+                lblMsg.Content = "The process has finished...";
+            }
+        }
+
+        private void loadFile(string filter)
+        {
+            ofd = new OpenFileDialog();
+            ofd.Filter = filter;
+        }
+    }
+}
